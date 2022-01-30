fd53ee0eced11632f3bc17f961432e87fd5de0d7
alternateved <alternateved@pm.me>
2022-01-30 21:10:51 +0100

    Add nix files

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "PureScript by Example";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    easy-purescript-nix-source = {
+      url = "github:justinwoo/easy-purescript-nix";
+      flake = false;
+    };
+  };
+
+  outputs = {self, nixpkgs, flake-utils, easy-purescript-nix-source}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        easy-ps = import easy-purescript-nix-source { inherit pkgs; };
+        inherit (easy-ps) purescript-language-server purs purty spago;
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs =  with pkgs; [
+            purescript-language-server
+            purs
+            purty
+            spago
+          ];
+        };
+      });
+}
