ce63a4b3032fdf9d9ec8aa17a16d047e7d470a30
Tomasz Hołubowicz <alternateved@pm.me>
2022-05-03 21:29:35 +0200

    Solve chapter 11

--- exercises/chapter11/test/Main.purs
+++ exercises/chapter11/test/Main.purs
@@ -28,7 +28,6 @@ main :: Effect Unit
 main =
   runTest do
     test "" success
-    {-  Move this block comment starting point to enable more tests
     suite "Exercises Group - The State Monad" do
       suite "testParens" do
         let
@@ -36,13 +35,15 @@ main =
           runTestParens expected str =
             test testName do
               Assert.equal expected $ testParens str
-            where testName = "str = \"" <> str <> "\""
+            where
+            testName = "str = \"" <> str <> "\""
         runTestParens true ""
         runTestParens true "(()(())())"
         runTestParens true "(hello)"
         runTestParens false ")"
         runTestParens false "(()()"
         runTestParens false ")("
+
     suite "Exercises Group - The Reader Monad" do
       suite "indents" do
         let
@@ -53,49 +54,54 @@ main =
             \    I am even more indented"
         test "should render with indentations" do
           Assert.equal expectedText
-            $ render $ cat
-              [ line "Here is some indented text:"
-              , indent $ cat
-                [ line "I am indented"
-                , line "So am I"
-                , indent $ line "I am even more indented"
+            $ render
+            $ cat
+                [ line "Here is some indented text:"
+                , indent $ cat
+                    [ line "I am indented"
+                    , line "So am I"
+                    , indent $ line "I am even more indented"
+                    ]
                 ]
-              ]
     suite "Exercises Group - The Writer Monad" do
       suite "sumArrayWriter" do
         test "should sum arrays" do
           Assert.equal (Additive 21)
-            $ execWriter $ do
-              sumArrayWriter [1, 2, 3]
-              sumArrayWriter [4, 5]
-              sumArrayWriter [6]
+            $ execWriter
+            $ do
+                sumArrayWriter [ 1, 2, 3 ]
+                sumArrayWriter [ 4, 5 ]
+                sumArrayWriter [ 6 ]
       suite "collatz" do
         let
           expected_11 =
-            Tuple 14 [11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+            Tuple 14 [ 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 ]
           expected_15 =
-            Tuple 17 [15, 46, 23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+            Tuple 17 [ 15, 46, 23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1 ]
         test "c = 11" do
           Assert.equal expected_11
             $ collatz 11
         test "c = 15" do
           Assert.equal expected_15
             $ collatz 15
+
     suite "Exercises Group - Monad Transformers" do
       suite "safeDivide" do
         test "should fail when dividing by zero" do
           Assert.equal (Left "Divide by zero!")
-            $ unwrap $ runExceptT $ safeDivide 5 0
+            $ unwrap
+            $ runExceptT
+            $ safeDivide 5 0
         test "should successfully divide for any other input" do
           Assert.equal (Right 2) $ unwrap $ runExceptT $ safeDivide 6 3
       suite "parser" do
         let
           runParser p s = unwrap $ runExceptT $ runWriterT $ runStateT p s
         test "should parse a string" do
-          Assert.equal (Right (Tuple (Tuple "abc" "def") ["The state is abcdef"]))
+          Assert.equal (Right (Tuple (Tuple "abc" "def") [ "The state is abcdef" ]))
             $ runParser (string "abc") "abcdef"
         test "should fail if string could not be parsed" do
-          Assert.equal (Left ["Could not parse"])
+          Assert.equal (Left [ "Could not parse" ])
             $ runParser (string "abc") "foobar"
       suite "indents with ReaderT and WriterT" do
         let
@@ -106,7 +112,8 @@ main =
             \    I am even more indented"
         test "should render with indentations" do
           Assert.equal expectedText
-            $ render' $ do
+            $ render'
+            $ do
                 line' "Here is some indented text:"
                 indent' $ do
                   line' "I am indented"
@@ -119,27 +126,37 @@ main =
         let
           runParser p s = unwrap $ runExceptT $ runWriterT $ runStateT p s
         test "should parse as followed by bs" do
-          Assert.equal (Right (Tuple (Tuple "aaabb" "cde") [
-            "The state is aaabbcde",
-            "The state is aabbcde",
-            "The state is abbcde",
-            "The state is bbcde",
-            "The state is bcde"]))
+          Assert.equal
+            ( Right
+                ( Tuple (Tuple "aaabb" "cde")
+                    [ "The state is aaabbcde"
+                    , "The state is aabbcde"
+                    , "The state is abbcde"
+                    , "The state is bbcde"
+                    , "The state is bcde"
+                    ]
+                )
+            )
             $ runParser asFollowedByBs "aaabbcde"
         test "should fail if first is not a" do
-          Assert.equal (Left ["Could not parse"])
+          Assert.equal (Left [ "Could not parse" ])
             $ runParser asFollowedByBs "bfoobar"
         test "should parse as and bs" do
-          Assert.equal (Right (Tuple (Tuple "babbaa" "cde") [
-            "The state is babbaacde",
-            "The state is abbaacde",
-            "The state is bbaacde",
-            "The state is baacde",
-            "The state is aacde",
-            "The state is acde"]))
+          Assert.equal
+            ( Right
+                ( Tuple (Tuple "babbaa" "cde")
+                    [ "The state is babbaacde"
+                    , "The state is abbaacde"
+                    , "The state is bbaacde"
+                    , "The state is baacde"
+                    , "The state is aacde"
+                    , "The state is acde"
+                    ]
+                )
+            )
             $ runParser asOrBs "babbaacde"
         test "should fail if first is not a or b" do
-          Assert.equal (Left ["Could not parse","Could not parse"])
+          Assert.equal (Left [ "Could not parse", "Could not parse" ])
             $ runParser asOrBs "foobar"
 
     suite "Exercises Group - The RWS Monad" do
@@ -148,8 +165,8 @@ main =
         runGame testGame = runRWS testGame env initialGameState
         env = GameEnvironment { debugMode: false, playerName: "Phil" }
 
-        playerHasAllItems (GameState {inventory}) = inventory == S.fromFoldable [Candle, Matches]
-        mapIsEmpty (GameState {items}) = M.isEmpty items
+        playerHasAllItems (GameState { inventory }) = inventory == S.fromFoldable [ Candle, Matches ]
+        mapIsEmpty (GameState { items }) = M.isEmpty items
         expectedLogs = ("You now have the Candle" : "You now have the Matches" : L.Nil)
 
       suite "adds all items to your inventory when cheating" do
@@ -165,5 +182,3 @@ main =
         runCheatTest "move and cheat" $ move 0 (-1) *> move 0 1 *> cheat
         runCheatTest "pickup matches and cheat" $ pickUp Matches *> cheat
         runCheatTest "pickup all, move, and cheat" $ pickUp Matches *> move 0 1 *> pickUp Candle *> cheat
-
--}
--- exercises/chapter11/test/MySolutions.purs
+++ exercises/chapter11/test/MySolutions.purs
@@ -2,4 +2,179 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader : Add your solutions to this file
+import Control.Alt ((<|>))
+import Control.Monad.Except (ExceptT, throwError)
+import Control.Monad.Reader (Reader, ReaderT, ask, local, runReader, runReaderT)
+import Control.Monad.State (StateT, execState, get, modify_, put)
+import Control.Monad.Writer (Writer, WriterT, execWriterT, runWriter, tell)
+import Data.Array (fold, some)
+import Data.Identity (Identity)
+import Data.Maybe (Maybe(..))
+import Data.Monoid (power)
+import Data.Monoid.Additive (Additive(..))
+import Data.Newtype (unwrap)
+import Data.String (Pattern(..), drop, joinWith, take)
+import Data.String.CodeUnits (stripPrefix, toCharArray)
+import Data.Traversable (sequence, traverse_)
+import Data.Tuple (Tuple)
+
+-- (Medium) A string of parentheses is balanced if it is obtained by either
+-- concatenating zero-or-more shorter balanced strings, or by wrapping a shorter
+-- balanced string in a pair of parentheses. Use the State monad and the traverse_
+-- function to write a function which tests whether or not a String of parentheses
+-- is balanced, by keeping track of the number of opening parentheses which have
+-- not been closed.
+
+testParens :: String -> Boolean
+testParens s =
+  compareParens
+    $ flip execState 0
+    $ traverse_ (\c -> modify_ $ countParens c) s'
+
+  where
+  s' = toCharArray s
+
+  compareParens :: Int -> Boolean
+  compareParens 0 = true
+  compareParens _ = false
+
+  countParens :: Char -> Int -> Int
+  countParens '(' parens | parens >= 0 = parens + 1
+  countParens ')' parens = parens - 1
+  countParens _ parens = parens
+
+-- (Easy) Write a function line which renders a function at the current indentation
+-- level. Hint: use the ask function to read the current indentation level. The
+-- power function from Data.Monoid may be helpful too.
+
+type Level = Int
+
+type Doc = Reader Level String
+
+line :: String -> Doc
+line s = do
+  level <- ask
+  pure $ (power "  " level) <> s
+
+-- (Easy) Use the local function to write a function which increases the
+-- indentation level for a block of code.
+
+indent :: Doc -> Doc
+indent = local (_ + 1)
+
+-- (Medium) Use the sequence function defined in Data.Traversable to write a
+-- function which concatenates a collection of documents, separating them with new
+-- lines.
+
+cat :: Array Doc -> Doc
+cat = pure <<< joinWith "\n" <=< sequence
+
+-- (Medium) Use the runReader function to write a function which renders a document
+-- as a String.
+
+render :: Doc -> String
+render = flip runReader 0
+
+-- (Medium) Rewrite the sumArray function above using the Writer monad and the
+-- Additive Int monoid from the monoid package.
+
+sumArrayWriter :: Array Int -> Writer (Additive Int) Unit
+sumArrayWriter = traverse_ \n -> tell $ Additive n
+
+-- (Medium) The Collatz function is defined on natural numbers n as n / 2 when n is
+-- even, and 3 * n + 1 when n is odd. For example, the iterated Collatz sequence
+-- starting at 10 is as follows: 10, 5, 16, 8, 4, 2, 1, ...
+
+collatz :: Int -> Tuple Int (Array Int)
+collatz c = runWriter $ clatz c 0
+  where
+  clatz :: Int -> Int -> Writer (Array Int) Int
+  clatz 1 i = do
+    tell [ 1 ]
+    pure i
+  clatz n i = do
+    tell [ n ]
+    if mod n 2 == 0 then clatz (n / 2) (i + 1)
+    else clatz (3 * n + 1) (i + 1)
+
+-- (Easy) Use the ExceptT monad transformer over the Identity functor to write a
+-- function safeDivide which divides two numbers, throwing an error (as the String
+-- "Divide by zero!") if the denominator is zero.
+
+safeDivide :: Int -> Int -> ExceptT String Identity Int
+safeDivide a b
+  | b == 0 = throwError "Divide by zero!"
+  | otherwise = pure $ a / b
+
+-- (Medium) Write a parser which matches a string as a prefix of the current state,
+-- or fails with an error message. Your parser should work as follows:
+
+-- > runParser (string "abc") "abcdef" (Right (Tuple (Tuple "abc" "def") ["The
+-- state is abcdef"]))
+
+type Errors = Array String
+
+type Log = Array String
+
+type Parser = StateT String (WriterT Log (ExceptT Errors Identity))
+
+split :: Parser String
+split = do
+  s <- get
+  tell [ "The state is " <> s ]
+  case s of
+    "" -> throwError [ "Empty string" ]
+    _ -> do
+      put (drop 1 s)
+      pure (take 1 s)
+
+string :: String -> Parser String
+string prefix = do
+  state <- get
+  tell [ "The state is " <> state ]
+  case stripPrefix (Pattern prefix) state of
+    Nothing -> throwError [ "Could not parse" ]
+    Just r -> put r *> pure prefix
+
+-- (Difficult) Use the ReaderT and WriterT monad transformers to reimplement the
+-- document printing library which we wrote earlier using the Reader monad.
+
+-- Instead of using line to emit strings and cat to concatenate strings, use the
+-- Array String monoid with the WriterT monad transformer, and tell to append a
+-- line to the result. Use the same names as in the original implementation but
+-- ending with an apostrophe (').
+
+type Level' = Int
+type Doc' = WriterT (Array String) (ReaderT Level' Identity) Unit
+
+line' :: String -> Doc'
+line' s = do
+  level <- ask
+  tell [ (power "  " level) <> s ]
+  pure unit
+
+indent' :: Doc' -> Doc'
+indent' = local (_ + 1)
+
+render' :: Doc' -> String
+render' = joinWith "\n" <<< unwrap <<< flip runReaderT 0 <<< execWriterT
+
+-- (Easy) Remove the calls to the lift function from your implementation of the
+-- string parser. Verify that the new implementation type checks, and convince
+-- yourself that it should.
+
+-- (Medium) Use your string parser with the some combinator to write a parser
+-- asFollowedByBs which recognizes strings consisting of several copies of the
+-- string "a" followed by several copies of the string "b".
+
+asFollowedByBs :: Parser String
+asFollowedByBs = do
+  as <- some $ string "a"
+  bs <- some $ string "b"
+  pure $ fold $ as <> bs
+
+-- (Medium) Use the <|> operator to write a parser asOrBs which recognizes strings
+-- of the letters a or b in any order.
+
+asOrBs :: Parser String
+asOrBs = fold <$> some (string "a" <|> string "b")
